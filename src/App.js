4765a911570bb41e1724c81f122f722e5a9f88b1
Aditya <adi454282@gmail.com>
2022-04-27 17:30:10 +0530

    Alert added with logo

--- src/App.js
+++ src/App.js
@@ -4,28 +4,48 @@ import './App.css';
 import Navbar from './component/Navbar';
 import TextForm from './component/TextForm';
 import { useState } from 'react';
+import Alert from './component/Alert';
 
 
 // let name = "Aditya"
 function App() {
   const [mode, setMode] = useState('light'); //defines dark mode is on or off
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message, type) =>{
+    
+  setAlert({
+    msg: message,
+    type: type
+  })
+  
+  setTimeout(() => {
+    setAlert(null);
+  }, 1500);
+  }
 
   const toggleMode = ()=>{
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
+      showAlert("dark mode enabled", "success")
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
+      showAlert("light mode enabled", "success")
+
     }
   }
   return (
    <>
    
 <Navbar Title="TextEdits" textAbout="About us" mode={mode} toggleMode={toggleMode}/> 
+
+<Alert alert={alert}/>
+
 <div className="container my-5">
-<TextForm heading="Text to analyis"/>
+<TextForm showAlert={showAlert} heading="Text to analyis"/>
 <button className='btn btn-seondary ' />
 </div>
 {/* <Navbar  />  */}
--- src/component/TextForm.js
+++ src/component/TextForm.js
@@ -6,26 +6,35 @@ export default function TextForm(props) {
         // console.log("text converted to Uppercase"+text);
         let newText = text.toUpperCase();
         setText(newText)
+        props.showAlert("Converted to UpperCase", "success");
     }
     
     const ChangetoLowercase = () =>{
         // console.log("text converted to LowerCase"+text);
         let newText = text.toLowerCase();
         setText(newText)
+        props.showAlert("Converted to LowerCase", "success");
+
     }
     const  ClrText = () =>{
         // console.log("text converted to LowerCase"+text);
         let newText = " ";
         setText(newText)
+        props.showAlert("Text is cleared", "success");
+
     }
     const  CopyText = () =>{
     var text = document.getElementById("myBox");
     text.select();
-    navigator.clipboard.writeText(text.value) ;       
+    navigator.clipboard.writeText(text.value) ; 
+    props.showAlert("Text copied to clipboard", "success");
+      
     }
     const  RemoveSpace = () =>{
       let newText = text.split(/[ ]+/);
       setText(newText.join(" "))
+      props.showAlert("Extra spaces removed", "success");
+
          
     }
     // const showDropdown = () =>{
@@ -36,6 +45,18 @@ export default function TextForm(props) {
         console.log("handleOnchange");
         setText(event.target.value)
     }
+
+    /* **********************************************************TO DO******************************************************************************************************* */
+  // const textSum =(text.split(" ").length)=> {
+    //   if(text.length==0){
+    //     text.split(" ").length == 0;
+    //   }
+    //   else{
+    //     text.split(" ").length == text.split(" ").length-1
+    //   }
+    // }
+    /* ***************************************************************************************************************************************************************** */
+
     const [text, setText] = useState(' ');
     // setText("Sometimes All i think about is you");
   return (
@@ -44,7 +65,7 @@ export default function TextForm(props) {
         <h1  style={{color: 'Orange'}}>{props.heading}</h1>
 <div className="mb-3">
   {/* <label htmlFor="exampleFormControlTextarea1" className="form-label">Enter Text</label> */}
-  <textarea className="form-control" value={text} onChange={handleOnchange} id="myBox" rows="8"></textarea>
+  <textarea className="form-control" value={text} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'#042743'}} onChange={handleOnchange} id="myBox" rows="8"></textarea>
 </div>
 
 <button className="btn btn-primary mx-1" onClick={ChangetoUppercase}>Convert Uppercase</button>
@@ -53,7 +74,7 @@ export default function TextForm(props) {
 <button className="btn btn-primary mx-1" onClick={CopyText}>Copy Text</button>
 <button className="btn btn-primary mx-1" onClick={RemoveSpace}>Remove extra Space</button>
 
-{/* ***************************************************************************************************************************************************************** */}
+{/* ***********************************************************TO DO****************************************************************************************************** */}
 <div className="dropdown">
   <a className="btn btn-secondary dropdown-toggle my-3 mx-1" href="/" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
     Dropdown link
@@ -75,7 +96,7 @@ export default function TextForm(props) {
            No. of minutes required to read the given text {0.008 * text.split(" ").length} 
         </p>
         <h2 style={{color: 'Orange'}}>Preview </h2>
-        <p style={{color: "green"}}> {text} </p>
+        <p style={{color: "green"}}>{text.length>0?text:"Enter something in the textbox above to preview it here"} </p>
 
     </div>
     </>
